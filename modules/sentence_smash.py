5eee2cfc3e3e839c9428b4ba6758833a1f5f501e
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-03 13:13:49 +0000

    refactor: programmatically remove all of the padded spaces

--- modules/sentence_smash.py
+++ modules/sentence_smash.py
@@ -1,8 +1,4 @@
 def smash(items: list[str]) -> str:
     if not isinstance(items, list):
         raise ValueError("❗️ Input should be a list")
-    if items == ["cat ", " eats"]:
-        return "cat eats"
-    if items == ["the", "world  ", " eats"]:
-        return "the world eats"
-    return " ".join(items)
+    return " ".join([item.strip() for item in items])
