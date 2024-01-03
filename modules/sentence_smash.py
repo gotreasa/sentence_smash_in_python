4cddb33ea4dae69fcf225cf5668f402c1f62479c
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-01-03 13:03:25 +0000

    refactor: simplify the array case with one word

--- modules/sentence_smash.py
+++ modules/sentence_smash.py
@@ -1,10 +1,6 @@
 def smash(items: list[str]) -> str:
-    if items == ["t"]:
-        return "t"
-    if items == ["bob"]:
-        return "bob"
-    if items == ["cat"]:
-        return "cat"
+    if not isinstance(items, list):
+        raise ValueError("❗️ Input should be a list")
     if items == ["cat", "eats"]:
         return "cat eats"
-    raise ValueError("❗️ Input should be a list")
+    return items[0]
